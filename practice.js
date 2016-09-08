a6a143d30ed11508e7f9d8cb227fb25470b9dd75
Mark Ferris <littleferris@gmail.com>
2016-09-07 22:12:35 -0600

    finished

--- practice.js
+++ practice.js
@@ -2,18 +2,27 @@
   // 1) What is the purpose of the 'this keyword'?
 
       //Answer
+      // It is a pointer
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
       //Answer
+      /*
+      1. implicit
+      1. explicit
+      3. new
+      4, default
+      */
 
   // 3) What is the difference between call and apply?
 
       //Answer
+      //call asigns method to the function, apply sets method and turns parameters to an array
 
   // 4) What does .bind do?
 
       //Answer
+      //it sets the method name to the function (this)
 
 
 //Next Problem
@@ -24,19 +33,39 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
+var user = {
+  username: 'littleferris',
+  email: 'littleferris@gmail',
+  getUsername: function () {
+    return this.username;
+  }
+}
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+user.getUsername();
 
 //Next Problem
 
 
 // Write the function definitions which will make the following function invocations function properly.
 
-  //Function Invocations Here
+  function Car(make, model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.move = 0;
+    this.moveCar = function() {
+      return this.move + 10;
+    };
+  }//Function Invocations Here
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
+
+
+function moveCar() {
+  return this.move + 10;
+}
 
 prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
@@ -69,7 +98,7 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.bind(myUser)(); //Fix this
 
 //Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function return?
 //Note(no tests)
